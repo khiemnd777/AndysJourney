8d1a88fd7ec583c4c1b95805a345616f0976b0ce
Khiemnd <khiemnd777@gmail.com>
2018-06-17 18:15:35 +0700

    fly me to the moon

--- Source/AndysJourney/Assets/Scripts/Player/AndyTranformer.cs
+++ Source/AndysJourney/Assets/Scripts/Player/AndyTranformer.cs
@@ -6,23 +6,48 @@ public class AndyTranformer : MonoBehaviour
 {
     public Transform andyPrefab;
     [SerializeField]
+    bool _waitForEffect;
+    [SerializeField]
     Animator _boomEffectPrefab;
+    SpriteRenderer _renderer;
+
+    void Start(){
+        _renderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
         {
-            if (andyPrefab == null || andyPrefab is Object && andyPrefab.Equals(null))
-                return;
-            if (_boomEffectPrefab != null && _boomEffectPrefab is Object && !_boomEffectPrefab.Equals(null))
-            {
-                var boomFx = Instantiate<Animator>(_boomEffectPrefab, transform.position, Quaternion.identity);
-                boomFx.gameObject.SetActive(true);
-                Destroy(boomFx.gameObject, boomFx.GetCurrentAnimatorStateInfo(0).length);
-            }
-            var beast = Instantiate<Transform>(andyPrefab, transform.position, Quaternion.identity);
-            beast.gameObject.SetActive(true);
-            Destroy(gameObject);
+            StartCoroutine(Transform());
         }
+    }
+
+    IEnumerator Transform()
+    {
+        if (andyPrefab == null || andyPrefab is Object && andyPrefab.Equals(null))
+            yield break;
+        // transform to beast
+        var andy = Instantiate<Transform>(andyPrefab, transform.position, Quaternion.identity);
+        var andyRenderer = andy.GetComponent<SpriteRenderer>();
+        // hidden for caching
+        andyRenderer.enabled = false;
+        // hidden the current sprite.
+        _renderer.enabled = false;
+        // active transformed effect.
+        if (_boomEffectPrefab != null && _boomEffectPrefab is Object && !_boomEffectPrefab.Equals(null))
+        {
+            var boomFx = Instantiate<Animator>(_boomEffectPrefab, transform.position, Quaternion.identity);
+            boomFx.gameObject.SetActive(true);
+            var tranformLength = boomFx.GetCurrentAnimatorStateInfo(0).length;
+            Destroy(boomFx.gameObject, tranformLength);
+            if(_waitForEffect)
+                yield return new WaitForSeconds(tranformLength);
+        }
+        // assure the beast must be actived.
+        andy.gameObject.SetActive(true);
+        // shown the beast after done.
+        andyRenderer.enabled = true;
+        Destroy(gameObject);
     }
 }
--- Source/AndysJourney/Assets/Scripts/Player/Beasts/BeastTranformer.cs
+++ Source/AndysJourney/Assets/Scripts/Player/Beasts/BeastTranformer.cs
@@ -6,23 +6,48 @@ public class BeastTranformer : MonoBehaviour
 {
     public Beast beastPrefab;
     [SerializeField]
+    bool _waitForEffect;
+    [SerializeField]
     Animator _boomEffectPrefab;
+    SpriteRenderer _renderer;
+
+    void Start(){
+        _renderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
         {
-            if (beastPrefab == null || beastPrefab is Object && beastPrefab.Equals(null))
-                return;
-            if (_boomEffectPrefab != null && _boomEffectPrefab is Object && !_boomEffectPrefab.Equals(null))
-            {
-                var boomFx = Instantiate<Animator>(_boomEffectPrefab, transform.position, Quaternion.identity);
-                boomFx.gameObject.SetActive(true);
-                Destroy(boomFx.gameObject, boomFx.GetCurrentAnimatorStateInfo(0).length);
-            }
-            var beast = Instantiate<Beast>(beastPrefab, transform.position, Quaternion.identity);
-            beast.gameObject.SetActive(true);
-            Destroy(gameObject);
+            StartCoroutine(Transform());
         }
+    }
+
+    IEnumerator Transform()
+    {
+        if (beastPrefab == null || beastPrefab is Object && beastPrefab.Equals(null))
+            yield break;
+        // transform to beast
+        var beast = Instantiate<Beast>(beastPrefab, transform.position, Quaternion.identity);
+        var beastRenderer = beast.GetComponent<SpriteRenderer>();
+        // hidden for caching
+        beastRenderer.enabled = false;
+        // hidden the current sprite.
+        _renderer.enabled = false;
+        // active transformed effect.
+        if (_boomEffectPrefab != null && _boomEffectPrefab is Object && !_boomEffectPrefab.Equals(null))
+        {
+            var boomFx = Instantiate<Animator>(_boomEffectPrefab, transform.position, Quaternion.identity);
+            boomFx.gameObject.SetActive(true);
+            var tranformLength = boomFx.GetCurrentAnimatorStateInfo(0).length;
+            Destroy(boomFx.gameObject, tranformLength);
+            if(_waitForEffect)
+                yield return new WaitForSeconds(tranformLength);
+        }
+        // assure the beast must be actived.
+        beast.gameObject.SetActive(true);
+        // shown the beast after done.
+        beastRenderer.enabled = true;
+        Destroy(gameObject);
     }
 }
