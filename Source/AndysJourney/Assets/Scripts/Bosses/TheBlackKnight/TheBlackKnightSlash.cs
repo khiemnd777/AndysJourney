8e56b150acc3e6e931c962c350157f06160416e5
Khiemnd <khiemnd777@gmail.com>
2018-09-01 22:00:09 +0700

    slash source code

--- Source/AndysJourney/Assets/Scripts/Bosses/TheBlackKnight/TheBlackKnightSlash.cs
+++ Source/AndysJourney/Assets/Scripts/Bosses/TheBlackKnight/TheBlackKnightSlash.cs
@@ -6,52 +6,85 @@ public class TheBlackKnightSlash : MonoBehaviour
 {
     [SerializeField]
     Animator _dustFxPrefab;
-	[SerializeField]
-	Animator _smashThunderFxPrefab;
-	[SerializeField]
-	Transform _smashThunderFxPoint;
-	[SerializeField]
-	AnimationClip[] _sequence;
-	[SerializeField]
-	float[] _velocities;
+    [SerializeField]
+    Animator _smashThunderFxPrefab;
+    [SerializeField]
+    Animator _dashingSmashingFireFxPrefab;
+    [SerializeField]
+    Transform _smashThunderFxPoint;
+    [SerializeField]
+    AnimationClip[] _sequence;
+    [SerializeField]
+    float[] _velocities;
 
-	Animator _anim;
-	Rigidbody2D _rigid;
+    Vector2 _lastPos;
+    Vector2 _startPos;
+    Animator _anim;
+    Rigidbody2D _rigid;
 
-	void Start(){
-		_anim = GetComponent<Animator>();
-		_rigid = GetComponent<Rigidbody2D>();
-		StartCoroutine(Play());
-	}
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _rigid = GetComponent<Rigidbody2D>();
+        StartCoroutine(Play());
+    }
 
-	public IEnumerator Play(){
-		var inx = 0;
-		foreach(var sq in _sequence){
-			_anim.Play(sq.name);
-			InstantiateTheDust(inx);
-			_rigid.velocity = Vector2.right * Time.fixedDeltaTime * _velocities[inx];
-			yield return new WaitForSeconds(sq.length);
-			_rigid.velocity = Vector2.zero;
-			inx++;
-		}
-		// _anim.Play("Idle", 0);
-	}
+    public IEnumerator Play()
+    {
+        var inx = 0;
+        foreach (var sq in _sequence)
+        {
+            _startPos = transform.position;
+            if (inx != _sequence.Length - 1)
+            {
+                InstantiateTheDust(inx);
+            }
+            else
+            {
+                transform.position = _lastPos;
+            }
+			if(inx == _sequence.Length - 2){
+				StartCoroutine(InstantiateDashingSmashingFire());
+			}
+            _anim.Play(sq.name);
+            _rigid.velocity = Vector2.right * Time.fixedDeltaTime * _velocities[inx];
+            yield return new WaitForSeconds(sq.length);
+            _rigid.velocity = Vector2.zero;
+            _lastPos = transform.position;
+            inx++;
+        }
+    }
 
-	IEnumerator Dash(){
-		_rigid.velocity = Vector2.right * Time.deltaTime * 25f;
-		yield return new WaitForFixedUpdate();
-	}
+    IEnumerator Dash()
+    {
+        _rigid.velocity = Vector2.right * Time.deltaTime * 25f;
+        yield return new WaitForFixedUpdate();
+    }
 
-	void InstantiateTheSmashThunderFx(){
-		var ins = Instantiate<Animator>(_smashThunderFxPrefab, _smashThunderFxPoint.position, Quaternion.identity);
-		Destroy(ins.gameObject, ins.GetCurrentAnimatorStateInfo(0).length);
-	}
+    IEnumerator InstantiateDashingSmashingFire()
+    {
+        var endTime = Time.time + _anim.GetCurrentAnimatorStateInfo(0).length;
+        while (Time.time <= endTime)
+        {
+			var ins = Instantiate<Animator>(_dashingSmashingFireFxPrefab, transform.position, Quaternion.identity);
+			Destroy(ins.gameObject, ins.GetCurrentAnimatorStateInfo(0).length);
+			yield return new WaitForSeconds(endTime/20f);
+        }
+    }
 
-	void InstantiateTheDust(int index){
-		var ins = Instantiate<Animator>(_dustFxPrefab, transform.position, Quaternion.identity);
-		if(index == _sequence.Length - 1){
-			ins.transform.localScale = Vector3.one * 1.45f;
-		}
-		Destroy(ins.gameObject, ins.GetCurrentAnimatorStateInfo(0).length);
-	}
+    void InstantiateTheSmashThunderFx()
+    {
+        var ins = Instantiate<Animator>(_smashThunderFxPrefab, _smashThunderFxPoint.position, Quaternion.identity);
+        Destroy(ins.gameObject, ins.GetCurrentAnimatorStateInfo(0).length);
+    }
+
+    void InstantiateTheDust(int index)
+    {
+        var ins = Instantiate<Animator>(_dustFxPrefab, transform.position, Quaternion.identity);
+        if (index == _sequence.Length - 1)
+        {
+            ins.transform.localScale = Vector3.one * 1.45f;
+        }
+        Destroy(ins.gameObject, ins.GetCurrentAnimatorStateInfo(0).length);
+    }
 }
