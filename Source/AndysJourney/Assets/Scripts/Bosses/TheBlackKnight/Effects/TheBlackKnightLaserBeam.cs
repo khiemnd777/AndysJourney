6270b1e4a9744b686fe5748da7282c5a52749692
Khiemnd <khiemnd777@gmail.com>
2018-10-07 22:43:44 +0700

    laser beam outline

--- Source/AndysJourney/Assets/Scripts/Bosses/TheBlackKnight/Effects/TheBlackKnightLaserBeam.cs
+++ Source/AndysJourney/Assets/Scripts/Bosses/TheBlackKnight/Effects/TheBlackKnightLaserBeam.cs
@@ -25,6 +25,10 @@ public class TheBlackKnightLaserBeam : MonoBehaviour
     [SerializeField]
     float _duration;
 
+    [SerializeField]
+    Transform _impactedLaserPrefab;
+    Transform _impactedLaser;
+
     LineRenderer _lr;
 
     void Start()
@@ -49,13 +53,79 @@ public class TheBlackKnightLaserBeam : MonoBehaviour
             var expPos = Vector3.Lerp(fromPos, toPos, _curve.Evaluate(t));
             var expDir = (expPos - _start.position).normalized;
             var hit = Physics2D.Raycast(_start.position, expDir, float.MaxValue, _layerMask);
-            if(hit.collider != null){
+            if (hit.collider != null)
+            {
                 var endPoint = hit.point;
+                InstantiateTheImpacted(endPoint, (BoxCollider2D)hit.collider);
                 _lr.SetPosition(1, new Vector2(endPoint.x, endPoint.y));
+                UpdateTheImpacted(endPoint, (BoxCollider2D)hit.collider);
             }
             yield return null;
         }
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.15f);
+        DestroyTheImpacted();
         StartCoroutine(RotateTheBeam());
+    }
+
+    void InstantiateTheImpacted(Vector3 position, BoxCollider2D collider)
+    {
+        if (_impactedLaser != null && _impactedLaser is Object && !_impactedLaser.Equals(null))
+        {
+            return;
+        }
+        var angle = GetHitSurfaceAngleOfBoxCollider2D(collider, position) - 90f;
+        var qt = Quaternion.AngleAxis(angle, Vector3.forward);
+        _impactedLaser = Instantiate<Transform>(_impactedLaserPrefab, position, qt);
+        _impactedLaser.localScale = _impactedLaser.localScale * 1.12f;
+        _impactedLaser.gameObject.SetActive(true);
+    }
+
+    void UpdateTheImpacted(Vector3 position, BoxCollider2D collider)
+    {
+        if (_impactedLaser == null || _impactedLaser is Object && _impactedLaser.Equals(null))
+        {
+            return;
+        }
+        var angle = GetHitSurfaceAngleOfBoxCollider2D(collider, position) - 90f;
+        var qt = Quaternion.AngleAxis(angle, Vector3.forward);
+        _impactedLaser.rotation = qt;
+        _impactedLaser.position = position;
+    }
+
+    void DestroyTheImpacted(){
+        if (_impactedLaser == null || _impactedLaser is Object && _impactedLaser.Equals(null))
+            return;
+        Destroy(_impactedLaser.gameObject);
+    }
+
+    float GetHitSurfaceAngleOfBoxCollider2D(BoxCollider2D collider, Vector2 hitPoint)
+    {
+        var bounds = collider.bounds;
+        var min = bounds.min;
+        var max = bounds.max;
+        var displacement = Vector3.zero;
+        // left
+        if (Mathf.Approximately(hitPoint.x, min.x) && Mathf.Clamp(hitPoint.y, min.y, max.y) == hitPoint.y)
+        {
+            displacement = new Vector3(min.x, max.y, min.z) - min;
+        }
+        // right
+        if (Mathf.Approximately(hitPoint.x, max.x) && Mathf.Clamp(hitPoint.y, min.y, max.y) == hitPoint.y)
+        {
+            displacement = max - new Vector3(max.x, min.y, max.z);
+        }
+        // up
+        if (Mathf.Approximately(hitPoint.y, max.y) && Mathf.Clamp(hitPoint.x, min.x, max.x) == hitPoint.x)
+        {
+            displacement = max - new Vector3(min.x, max.y, min.z);
+        }
+        // bottom
+        if (Mathf.Approximately(hitPoint.y, min.y) && Mathf.Clamp(hitPoint.x, min.x, max.x) == hitPoint.x)
+        {
+            displacement = new Vector3(max.x, min.y, max.z) - min;
+        }
+        var ninety = Vector3.Cross(Vector3.forward, displacement);
+        var angle = Mathf.Atan2(ninety.y, ninety.x) * Mathf.Rad2Deg;
+        return angle;
     }
 }
